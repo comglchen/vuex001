c6cb7967b719047e9f8fdab25d5b33c9b5235c78
gl-Chen <zdcs0705@163.com>
2022-09-08 16:42:10 +0800

    路由demo1

--- vuexlearn1/src/App.vue
+++ vuexlearn1/src/App.vue
@@ -1,41 +1,49 @@
 <template>
-  <div class="mainContait">
-    <div class="leftSide">
-      <span>
-        <router-link to="Home">goto Home component</router-link>
-      </span>
-      <span>
-        <router-link to="About">goto About component</router-link>
-      </span>
+  <!DOCTYPE html>
+  <html lang="">
+    <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    </head>
+    <div class="mainContait">
+      <div class="leftSide">
+        <div class="linkA">
+          <router-link class="routerLink" to="Home" active-class="active"
+            >返回首页</router-link
+          >
+        </div>
+        <div class="linkA">
+          <router-link class="routerLink" to="About" active-class="active"
+            >关于我们</router-link
+          >
+        </div>
+      </div>
+      <div class="viewComponent">
+        <router-view></router-view>
+      </div>
     </div>
-    <div class="viewComponent">
-      <router-view></router-view>
-    </div>
-  </div>
+  </html>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
 a {
   text-decoration: none;
 }
+
 .leftSide {
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: rgb(131, 97, 56);
-  width: 150px;
-  height: 800px;
+  width: 260px;
+  height: 1000px;
 }
-span {
-  width: 100px;
-  height: 50px;
-  background-color: antiquewhite;
-  margin-top: 10px;
-}
+
 .mainContait {
   display: flex;
   flex-direction: row;
@@ -44,5 +52,37 @@ span {
   width: 100%;
   background-color: aquamarine;
   margin-left: 10px;
+}
+.active {
+  background-color: rgb(11, 141, 56);
+}
+/* .router-link-exact-active {
+  background-color: orange;
+  color: #fff;
+} */
+
+/* .router-link-exact-active {
+  border-bottom: 4px solid red;
+} */
+
+.routerLink {
+  font-size: 20px;
+  color: rgb(244, 246, 249);
+  line-height: 38px;
+  text-align: center;
+  width: 150px;
+  height: 38px;
+}
+.linkA {
+  text-align: center;
+  margin-top: 5px;
+  width: 150px;
+  height: 38px;
+
+  border-radius: 5px;
+  background-color: rgba(240, 248, 255, 0.505);
+}
+.linkA:visited {
+  background-color: green;
 }
 </style>
--- vuexlearn1/src/main.js
+++ vuexlearn1/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 import router from './router'
 new Vue({
     el: '#app',
